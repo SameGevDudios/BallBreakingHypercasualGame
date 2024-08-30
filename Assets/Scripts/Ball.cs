5bc3908d7951143c98e19e2fae39697f131a1f0a
SameGevDudios <magomed.chapaev1@gmail.com>
2024-08-31 01:23:01 +0300

    Ball Animation
    Created test scene
    Created procedural ball animation (vertical movement, x and y scale)

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void Start()
+    {
+        DeactivateFrontBalls();
+        DeactivateBackBalls();
+        for (int i = 0; i < 2; i++)
+        {
+            ChangeColor();
+        }
+        _startPosition = transform.position;
+        StartCoroutine(MovingVertically());
+        StartCoroutine(ScalingBall());
+    }
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ChangeColor();
+        }
+#endif
+    }
+    private bool _frontSide;
+    [Tooltip("Sequence: Red, Blue, Yellow, Green")]
+    [SerializeField] private GameObject[] _frontBall, _backBall;
+    private int _colorIndex;
+    private int _previousColorIndex;
+    private void ChangeColor()
+    {
+        while(_colorIndex == _previousColorIndex)
+        {
+            _colorIndex = Random.Range(0, 4); // 0 - red, 1 - blue, 2 - yellow, 3 - green
+        }
+        if (_frontSide)
+        {
+            DeactivateFrontBalls();
+            _frontBall[_colorIndex].SetActive(true);
+        }
+        else
+        {
+            DeactivateBackBalls();
+            _backBall[_colorIndex].SetActive(true);
+        }
+        _frontSide = !_frontSide;
+        _previousColorIndex = _colorIndex;
+    }
+    public void ChangeColor(int colorIndex)
+    {
+        if (_frontSide)
+        {
+            DeactivateFrontBalls();
+            _frontBall[colorIndex].SetActive(true);
+        }
+        else
+        {
+            DeactivateBackBalls();
+            _backBall[colorIndex].SetActive(true);
+        }
+        _frontSide = !_frontSide;
+    }
+    private void DeactivateFrontBalls()
+    {
+        foreach (GameObject ball in _frontBall)
+            ball.SetActive(false);
+    }
+    private void DeactivateBackBalls()
+    {
+        foreach (GameObject ball in _backBall)
+            ball.SetActive(false);
+    }
+
+    [SerializeField] private AnimationCurve _verticalVelocity,
+        _horizontalScale, _verticalScale;
+    [SerializeField] private float _jumpSpeed = 1, _jumpHeight = 1;
+    private Vector3 _startPosition;
+    private IEnumerator MovingVertically()
+    {
+        float currentTime = 0;
+        float totalTime = _verticalVelocity.keys[_verticalVelocity.keys.Length - 1].time;
+        while (currentTime < totalTime)
+        {
+            transform.position = _startPosition + Vector3.up * _verticalVelocity.Evaluate(currentTime) * _jumpHeight;
+            currentTime += Time.deltaTime * _jumpSpeed;
+            yield return new WaitForEndOfFrame();
+        }
+        StartCoroutine(MovingVertically());
+    }
+    private IEnumerator ScalingBall()
+    {
+        float currentTime = 0;
+        float totalTime = _horizontalScale.keys[_horizontalScale.keys.Length - 1].time;
+        while(currentTime < totalTime)
+        {
+            float xScale = _horizontalScale.Evaluate(currentTime);
+            float yScale = _verticalScale.Evaluate(currentTime);
+            transform.localScale = Vector3.forward + Vector3.right * xScale + Vector3.up * yScale;
+            currentTime += Time.deltaTime * _jumpSpeed;
+            yield return new WaitForEndOfFrame();
+        }
+        StartCoroutine(ScalingBall());
+    }
+}
